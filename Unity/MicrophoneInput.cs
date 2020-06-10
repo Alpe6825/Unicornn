85352af658d481e0d1024159f0055451d319746c
Yohavier <40822700+Yohavier@users.noreply.github.com>
2020-06-10 17:36:56 +0200

    Micro Recording
    -recording Time is 5sec
    -Press R to start rec
    -Press R to stop rec
    -samples is a float array with the recorded data
    -!! nicht serializable machen weil sonst packt unity das nicht mehr so wirklich mit dem array

---
+++ Unity/MicrophoneInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput : MonoBehaviour
+{
+    private float[] samples;
+    [SerializeField]
+    private string selectedDevice;
+    private bool isRecording;
+    private AudioSource source;
+
+    private void Start()
+    {
+        selectedDevice = Microphone.devices[0].ToString();
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isRecording)
+                StartRecording();
+            else
+                StopRecording();
+        }
+    }
+
+    private void StopRecording()
+    {
+        isRecording = false;
+        Microphone.End(selectedDevice);
+        source.Play();
+
+        samples = new float[source.clip.samples * source.clip.channels];
+        source.clip.GetData(samples, 0);
+    }
+
+    void StartRecording()
+    {
+        isRecording = true;
+        source.clip = Microphone.Start(selectedDevice, true, 5, AudioSettings.outputSampleRate);
+        source.Play();
+    }
+}
