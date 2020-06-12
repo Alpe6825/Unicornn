622ce6a89fa80fa83de1b9fe8d27c6c4ae1d5e55
larajb <64007209+larajb@users.noreply.github.com>
2020-06-12 18:50:25 +0200

    trial drawing waveform in scene

--- Unity/MicrophoneInput.cs
+++ Unity/MicrophoneInput.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(RawImage))]
 public class MicrophoneInput : MonoBehaviour
 {
-    private enum Word { none = 0, first = 1, second = 2, third = 3}
+    private enum Word { none = 0, first = 1, second = 2, third = 3 }
     private Word word;
     private int wordCount;
     private float[] samples;
@@ -21,6 +23,15 @@ public class MicrophoneInput : MonoBehaviour
     private List<float> firstWord = new List<float>();
     private List<float> secondWord = new List<float>();
     private List<float> thirdWord = new List<float>();
+
+    //variables for audio waveform drawing
+    private int width = 500;
+    private int height = 100;
+    private Color backgroundColor = Color.black;
+    private Color waveformColor = Color.green;
+    private int size = 2048;
+    private Color[] blank;
+    private Texture2D texture;
 
     private void Start()
     {
@@ -94,16 +105,51 @@ public class MicrophoneInput : MonoBehaviour
         Debug.Log("first word lenght: " + firstWord.Count().ToString() + "\n");
         Debug.Log("second word lenght: " + secondWord.Count().ToString() + "\n");
         Debug.Log("third word lenght: " + thirdWord.Count().ToString() + "\n");
+
+
+        //initialization for audio waveform drawing
+        texture = new Texture2D(width, height);
+        GetComponent<RawImage>().texture = texture;
+        blank = new Color[width * height];
+        
+        //create blank screen image
+        for(int a = 0; a<size; a++)
+        {
+            blank[a] = backgroundColor;
+        }
+
+        StartCoroutine(UpdateWaveForm());
+    }
+
+    IEnumerator UpdateWaveForm()
+    {
+        //refresh display each 100ms
+        while(true)
+        {
+            GetCurWave();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void GetCurWave()
+    {
+        texture.SetPixel(blank, 0);
+        source.GetOutputData(samples, 0);
+        for(int i = 0; i<size; i++)
+        {
+            texture.SetPixel((int)(width * i / size), (int)(height * (samples[i] + 1f) / 2f), waveformColor);
+        }
+        texture.Apply();
     }
 
     private void WriteToWordList(float loudness, List<float> buffer)
     {
-        if(loudness > 15 && word == Word.none)
+        if (loudness > 15 && word == Word.none)
         {
             wordCount++;
             word = (Word)wordCount;
         }
-        else if(word != Word.none && loudness < 15)
+        else if (word != Word.none && loudness < 15)
         {
             word = Word.none;
         }
